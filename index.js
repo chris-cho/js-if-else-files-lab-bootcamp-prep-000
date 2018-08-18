ff6e7ea0aaa6f738bf5fdf559f7e9deb12c92fcc
Hyun sang Cho <hyunsangc@gmail.com>
2018-08-18 01:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ else{
   console.log("Expected the name to be Susan");
 }
 
-if (name === "Susan"){
-  console.log("The name is correct");
-}
-else{
-  console.log("Expected the name to be Susan");
-}
-
-
 // Don't worry about this
 module.exports = { name, height
 }
